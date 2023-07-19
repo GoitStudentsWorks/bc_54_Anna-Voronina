54e4b370b35b38bb7a52a3a3181ea0d18ef6181b
Anna-Voronina <anna.vs2512@gmail.com>
2023-07-19 09:47:05 +0300

    Create global slice

---
+++ src/services/redux/global/globalSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isModalAddTransactionOpen: false,
+  isModalLogoutOpen: false,
+  isLoading: false,
+};
+
+const globalSlice = createSlice({
+  name: global,
+  initialState,
+  reducers: {
+    openModalAddTransaction(state) {
+      state.isModalAddTransactionOpen = true;
+    },
+    closeModalAddTransaction(state) {
+      state.isModalAddTransactionOpen = false;
+    },
+    openModalLogout(state) {
+      state.isModalLogoutOpen = true;
+    },
+    closeModalLogout(state) {
+      state.isModalLogoutOpen = false;
+    },
+    setIsLoading(state) {
+      state.isLoading = true;
+    },
+    unsetIsLoading(state) {
+      state.isLoading = false;
+    },
+  },
+});
+
+export const globalReducer = globalSlice.reducer;
+export const {
+  openModalAddTransaction,
+  closeModalAddTransaction,
+  openModalLogout,
+  closeModalLogout,
+  setIsLoading,
+  unsetIsLoading,
+} = globalSlice.actions;
