714f7a1271de82125b5f1f2853538488306a7a11
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-07-19 15:09:45 +0300

    feat:adds media

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -30,6 +30,7 @@ import {
   delTransactionThunk,
 } from 'redux/transaction/transactionOperations';
 import { Button } from 'components/Button/Button';
+import MediaQuery from 'react-responsive/types/Component';
 // import { BtnAddTransaction } from 'components/BtnAddTransaction/BtnAddTransaction';
 // import { setUpdatedTransaction } from 'redux/global/slice';
 
@@ -55,49 +56,105 @@ const Transactions = () => {
     dispatch(delTransactionThunk(id));
   };
 
-  return (
-    <ul>
-      {sortedTransactions.map(transaction => {
-        return (
-          <li key={transaction.id}>
-            <ul>
-              <li>
-                <h4>Date</h4>
-                <p>{transaction.date}</p>
+  return transactions.length ? (
+    <>
+      <MediaQuery maxWidth={767}>
+        <ul>
+          {sortedTransactions.map(transaction => {
+            return (
+              <li key={transaction.id}>
+                <ul>
+                  <li>
+                    <h4>Date</h4>
+                    <p>{transaction.date}</p>
+                  </li>
+                  <li>
+                    <h4>Type</h4>
+                    <p>{transaction.type || '-'}</p>
+                  </li>
+                  <li>
+                    <h4>Category</h4>
+                    <p>{transaction.category}</p>
+                  </li>
+                  <li>
+                    <h4>Comment</h4>
+                    <p>{transaction.comment}</p>
+                  </li>
+                  <li>
+                    <h4>Sum</h4>
+                    <p>{transaction.sum}</p>
+                  </li>
+                  <li>
+                    <Button
+                      type="button"
+                      onClick={handleDeleteTransaction(transaction.id)}
+                      text="Delete"
+                    />
+                    <Button
+                      type="button"
+                      onClick={() => console.log(1)}
+                      text="Edit" // svg in text prop
+                    />
+                  </li>
+                </ul>
               </li>
-              <li>
-                <h4>Type</h4>
-                <p>{transaction.type || '-'}</p>
-              </li>
-              <li>
-                <h4>Category</h4>
-                <p>{transaction.category}</p>
-              </li>
-              <li>
-                <h4>Comment</h4>
-                <p>{transaction.comment}</p>
-              </li>
-              <li>
-                <h4>Sum</h4>
-                <p>{transaction.sum}</p>
-              </li>
-              <div>
-                <Button
-                  type="button"
-                  onClick={handleDeleteTransaction(transaction.id)}
-                  text="Delete"
-                />
-                <Button
-                  type="button"
-                  onClick={() => console.log(1)}
-                  text="Edit" // svg in text prop
-                />
-              </div>
-            </ul>
-          </li>
-        );
-      })}
-    </ul>
+            );
+          })}
+        </ul>
+      </MediaQuery>
+
+      <MediaQuery minWidth={768}>
+        <tbody>
+          <thead>
+            <th>Date</th>
+            <th>Type</th>
+            <th>Category</th>
+            <th>Comment</th>
+            <th>Sum</th>
+          </thead>
+          {sortedTransactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td>
+                  <h4>Date</h4>
+                  <p>{transaction.date}</p>
+                </td>
+                <td>
+                  <h4>Type</h4>
+                  <p>{transaction.type || '-'}</p>
+                </td>
+                <td>
+                  <h4>Category</h4>
+                  <p>{transaction.category}</p>
+                </td>
+                <td>
+                  <h4>Comment</h4>
+                  <p>{transaction.comment}</p>
+                </td>
+                <td>
+                  <h4>Sum</h4>
+                  <p>{transaction.sum}</p>
+                </td>
+                <td>
+                  <Button
+                    type="button"
+                    onClick={handleDeleteTransaction(transaction.id)}
+                    text="Delete"
+                  />
+                  <Button
+                    type="button"
+                    onClick={() => console.log(1)}
+                    text="Edit" // svg in text prop
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </MediaQuery>
+    </>
+  ) : (
+    <h2>No transactions yet</h2>
   );
 };
 
