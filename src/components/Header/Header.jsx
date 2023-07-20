e4a8fa53f8f45de3f5b13c1d62346f181b78d12f
Александр Пикуш <pikush.oleksandr@gmail.com>
2023-07-20 16:56:20 +0300

    add username

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,8 +16,10 @@ import { selectIsModalLogoutOpen } from 'redux/global/globalSelectors';
 import { ModalLogOut } from 'components/ModalLogOut/ModalLogOut';
 import { openModalLogout } from 'redux/global/globalSlice';
 import { HeaderLogo } from 'components/HeaderLogo/HeaderLogo';
+import { selectUser } from 'redux/auth/authSelectors';
 
 export const Header = () => {
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
   const isModalOpen = useSelector(selectIsModalLogoutOpen);
 
@@ -29,7 +31,7 @@ export const Header = () => {
         </Link>
 
         <ExitContainer>
-          <NameWrap>Name</NameWrap>
+          <NameWrap>{user.username}</NameWrap>
           <SvgStick xmlns="http://www.w3.org/2000/svg">
             <path d="M1 0V30" stroke="white" strokeOpacity="0.6" />
           </SvgStick>
