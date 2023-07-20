726ddd022e18dcc7d5c02b82dcc1ad211ae57f11
Vyachuk <Slavkoka045@gmail.com>
2023-07-20 17:41:17 +0300

    add modal

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,3 +1,34 @@
+import { Button } from 'components/Button/Button';
+import { Field, Form, Formik } from 'formik';
+import { ModalAddWrapper } from './ModalAddTransaction.styled';
+
 export const ModalAddTransaction = () => {
-  return <div>Hello from modal</div>;
+  const initialValue = {
+    count: '',
+    date: '',
+    comment: '',
+  };
+  const handleSubmit = (value, { resetForm }) => {
+    console.log(value);
+    resetForm();
+  };
+  return (
+    <ModalAddWrapper>
+      <h3>Add transaction</h3>
+      <div>
+        <span>Income</span>
+        <span></span>
+        <span>Expense</span>
+      </div>
+      <Formik onSubmit={handleSubmit} initialValue={initialValue}>
+        <Form>
+          <Field type="number" name="count" />
+          <Field type="data" name="date" />
+          <Field type="text" name="comment" />
+          <Button text="ADD" type="submit" />
+          <Button text="CANCEL" variant="secondary" />
+        </Form>
+      </Formik>
+    </ModalAddWrapper>
+  );
 };
