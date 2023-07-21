90e092d338d8d32b4e2526ba819ca97ad9932ca8
Anna-Voronina <anna.vs2512@gmail.com>
2023-07-22 01:11:56 +0300

    Update currency styles

--- src/components/CurrencyTable/CurrencyTable.styled.js
+++ src/components/CurrencyTable/CurrencyTable.styled.js
@@ -8,23 +8,37 @@ export const CurrencyChartWrapper = styled.div`
   font-size: ${({ theme }) => theme.fontSizes.m};
   line-height: ${({ theme }) => theme.lineHeights.normal}; */
   /* padding: 0px; */
-
 `;
 
 export const CurrencyTableStyled = styled.table`
-background-color: #2F15B0;
-  color: #fff;
+  color: ${({ theme }) => theme.colors.primaryFont};
   font-style: ${({ theme }) => theme.fonts.regular};
   font-size: ${({ theme }) => theme.fontSizes.m};
   line-height: ${({ theme }) => theme.lineHeights.normal};
   width: 100%;
+  margin: 0 auto;
   border-collapse: collapse;
+  thead {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+  thead > tr {
+    height: 56px;
+    display: flex;
+    gap: 50px;
+    justify-content: space-between;
+    padding-left: 62px;
+  }
+  th {
+    width: 77px;
+  }
+  td {
+    text-align: center;
+  }
 `;
 
 export const CurrencyTableHead = styled.thead`
-  
   th {
-    background-color: rgba(255, 255, 255, 0.2);
+    max-width: 77px;
     height: 50px;
     font-weight: ${({ theme }) => theme.fontWeights.semiBold};
   }
@@ -32,27 +46,23 @@ export const CurrencyTableHead = styled.thead`
   th::before,
   th::after {
     content: '';
-   
   }
 
   th::before {
-    
   }
 
   th::after {
-    
   }
-  
 `;
 
 export const CurrencyTr = styled.tr`
-/* margin-left: 0px; */
-font-weight: ${({ theme }) => theme.fontWeights.regular};
-`
+  /* margin-left: 0px; */
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+`;
 
 export const CurrencyTh = styled.th`
-/* margin-left: 0px; */
-`
+  /* margin-left: 0px; */
+`;
 export const CurrencyTableBody = styled.tbody`
   background-color: ${({ theme }) => theme.colors.blue};
   /* padding-left: 20px;
