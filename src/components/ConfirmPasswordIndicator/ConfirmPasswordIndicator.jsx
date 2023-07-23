45b14fdd84e0e9f52ceb1327381e5ed7f9511072
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-07-23 09:56:17 +0300

    add fixPropType

--- src/components/ConfirmPasswordIndicator/ConfirmPasswordIndicator.jsx
+++ src/components/ConfirmPasswordIndicator/ConfirmPasswordIndicator.jsx
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { IndicatorWrapper } from './ConfirmPasswordIndicator.styled';
 
 export const ConfirmPasswordIndicator = ({ passwordsMatch, values: { confirmPassword } }) => {
+  console.log(passwordsMatch);
   const width = passwordsMatch ? '100%' : '50%';
   return confirmPassword && <IndicatorWrapper passwordsMatch={passwordsMatch} style={{ width }} />;
+};
+
+ConfirmPasswordIndicator.propTypes = {
+  passwordsMatch: PropTypes.bool.isRequired,
 };
